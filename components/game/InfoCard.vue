86dad8dafa5a553114681a952ed4c390daeed2ba
mendmania <mendim@gjirafa.com>
2023-12-10 00:02:44 +0100

    [fixs] on card, readability

--- components/game/InfoCard.vue
+++ components/game/InfoCard.vue
@@ -8,14 +8,16 @@ const props = defineProps({
 </script>
 
 <template>
-  <div
-    class="w-full flex items-center max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 my-10"
-  >
-    <h5 class="text-xl font-medium text-gray-500 dark:text-gray-400 mr-3">
-      Room code:
-    </h5>
-    <div class="flex items-baseline text-gray-900 dark:text-white">
-      <span class="text-5xl font-extrabold tracking-tight">{{ text }}</span>
+  <div class="w-full flex justify-center">
+    <div
+      class="flex items-center max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 my-10 mx-4"
+    >
+      <h5 class="text-xl font-medium text-gray-500 dark:text-gray-400 mr-3">
+        Room code:
+      </h5>
+      <div class="flex items-baseline text-gray-900 dark:text-white">
+        <span class="text-5xl font-extrabold tracking-tight">{{ text }}</span>
+      </div>
     </div>
   </div>
 </template>
