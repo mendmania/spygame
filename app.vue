3e6590fd7866b1e9077501e1cdaa72ea0ed5b04a
mendmania <mendim@gjirafa.com>
2023-12-17 18:23:34 +0100

    [fixes] seo

--- app.vue
+++ app.vue
@@ -2,20 +2,20 @@
 import { onMounted } from "vue";
 
 useHead({
-  title: "Spyfall Game",
+  title: "Virtual Board Zone - Your Ultimate Online Board Game Destination",
   htmlAttrs: { lang: "en" },
   meta: [
     {
       hid: "description",
       name: "description",
       content:
-        "Spyfall is a thrilling board game of deception. Discover who the spy is before it’s too late! Learn the rules, strategies, and enjoy hours of espionage fun.",
+        "Enter the world of Virtual Board Zone for an unparalleled online board gaming experience. Explore our wide range of games, from classics to the latest hits. Play, connect, and enjoy!",
     },
     {
       hid: "keywords",
       name: "keywords",
       content:
-        "Spyfall, board game, deception, spy, party game, social deduction, strategy game",
+        "online board games, virtual board games, multiplayer games, family board games, strategy games, card games, Virtual Board Zone",
     },
     {
       hid: "og:type",
@@ -25,42 +25,47 @@ useHead({
     {
       hid: "og:title",
       property: "og:title",
-      content: "Spyfall Board Game - Uncover the Deception",
+      content: "Virtual Board Zone - Connect & Play",
     },
     {
       hid: "og:description",
       property: "og:description",
       content:
-        "Spyfall is a thrilling board game of deception. Discover who the spy is before it’s too late! Learn the rules, strategies, and enjoy hours of espionage fun.",
+        "Discover a wide variety of online board games at Virtual Board Zone. Connect with friends or make new ones while enjoying thrilling and strategic gameplay.",
     },
     {
       hid: "og:image",
       property: "og:image",
-      content: "https://spyfallgame.com/SpyfallBanner.jpg",
+      content: "https://virtualboardzone.com/HomeBanner.jpg",
     },
-    { hid: "og:url", property: "og:url", content: "spyfallgame.com" },
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: "https://virtualboardzone.com",
+    },
     {
       hid: "twitter:card",
       name: "twitter:card",
-      content: "https://spyfallgame.com/SpyfallBanner.jpg",
+      content: "https://virtualboardzone.com/HomeBanner.jpg",
     },
     {
       hid: "twitter:title",
       name: "twitter:title",
-      content: "Spyfall Board Game - Uncover the Deception",
+      content: "Virtual Board Zone - Connect & Play",
     },
     {
       hid: "twitter:description",
       name: "twitter:description",
       content:
-        "Spyfall is a thrilling board game of deception. Discover who the spy is before it’s too late! Learn the rules, strategies, and enjoy hours of espionage fun.",
+        "Dive into the exciting world of online board games with Virtual Board Zone. Enjoy a variety of games, from strategy to family favorites, in an interactive online setting.",
     },
     {
       hid: "twitter:image",
       name: "twitter:image",
-      content: "https://spyfallgame.com/SpyfallBanner.jpg",
+      content: "https://virtualboardzone.com/HomeBanner.jpg",
     },
   ],
+  // Additional tags for canonical URL, hreflang (if applicable), and schema markup
 });
 
 onMounted(() => {});
