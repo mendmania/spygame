80f8b8706402c22cd7472194c5fcbc5a16a8e0a7
mendmania <mendim@gjirafa.com>
2023-12-24 22:57:34 +0100

    [minor change]

--- components/Landing.vue
+++ components/Landing.vue
@@ -50,6 +50,7 @@ const createRoom = () => {
 };
 
 const joinRoom = async () => {
+  alert(1);
   const userData = prepareUserToJoinGame();
 
   const roomId = roomIdInput.value.toUpperCase();
@@ -125,6 +126,7 @@ const onUsernameInput = ($e) => {
           required
         />
         <button
+          @keydown.enter="joinRoom"
           @click="joinRoom"
           class="text-white absolute end-7 bottom-2.5 bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
